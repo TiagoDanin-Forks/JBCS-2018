f4acd360f4cfb8344948980864d724035dc7ed9a
Felipe Fronchetti <fronchetti@usp.br>
2018-07-14 10:47:06 -0300

    Updating days percentage

---
+++ Scripts/days_process_pull_request.R
@@ -0,0 +1,67 @@
+# install.packages("reshape2")
+# install.packages("scales")
+library(reshape2)
+library(scales)
+library(plyr)
+par(xpd=TRUE) # permite a legenda fora do texto
+
+setwd("/home/fronchetti/Documentos/JBCS-2018") # Working directory
+# op <- par(oma=c(0,0,5,0), mfrow=c(3,2))
+
+pulls_summary <- read.csv("Dataset/atom/merged_pull_requests_summary.csv", colClasses=c(NA, NA, NA, NA, NA, NA, "Date", NA, NA, NA, NA, NA))
+internals <- subset(pulls_summary, user_type == "Internals")
+externals <- subset(pulls_summary, user_type == "Externals")
+internals_sum <- count(internals, "user_login")
+externals_sum <- count(externals, "user_login")
+mar.default <- c(4,1,4,1) + 0.1
+par(mar = mar.default + c(0, 5, 0, 0)) 
+boxplot(externals_sum$freq, internals_sum$freq, ylim=c(0, 103), xlab="# Occurrences", las = 1, outline = FALSE, cex.lab=2, cex.axis = 1.5, horizontal = TRUE, margin = list(l = 10, r = 10, b = 0, t = 0), col=(c("#E6E6E6", "#727272")))
+legend(26, 3, c("Internals", "Externals"), col=c("#727272", "#E6E6E6"), ncol=2, cex=1.7, bty = "n", fill = c("#727272", "#E6E6E6"))
+
+pulls_summary <- read.csv("Dataset/electron/merged_pull_requests_summary.csv", colClasses=c(NA, NA, NA, NA, NA, NA, "Date", NA, NA, NA, NA, NA))
+internals <- subset(pulls_summary, user_type == "Internals")
+externals <- subset(pulls_summary, user_type == "Externals")
+internals_sum <- count(internals, "user_login")
+externals_sum <- count(externals, "user_login")
+mar.default <- c(4,1,4,1) + 0.1
+par(mar = mar.default + c(0, 5, 0, 0)) 
+boxplot(externals_sum$freq, internals_sum$freq, ylim=c(0, 103), xlab="# Occurrences", las = 1, outline = FALSE, cex.lab=2, cex.axis = 1.5, horizontal = TRUE, margin = list(l = 10, r = 10, b = 0, t = 0), col=(c("#E6E6E6", "#727272")))
+legend(26, 3, c("Internals", "Externals"), col=c("#727272", "#E6E6E6"), ncol=2, cex=1.7, bty = "n", fill = c("#727272", "#E6E6E6"))
+
+pulls_summary <- read.csv("Dataset/git-lfs/merged_pull_requests_summary.csv", colClasses=c(NA, NA, NA, NA, NA, NA, "Date", NA, NA, NA, NA, NA))
+internals <- subset(pulls_summary, user_type == "Internals")
+externals <- subset(pulls_summary, user_type == "Externals")
+internals_sum <- count(internals, "user_login")
+externals_sum <- count(externals, "user_login")
+mar.default <- c(4,1,4,1) + 0.1
+par(mar = mar.default + c(0, 5, 0, 0)) 
+boxplot(externals_sum$freq, internals_sum$freq, ylim=c(0, 103), xlab="# Occurrences", las = 1, outline = FALSE, cex.lab=2, cex.axis = 1.5, horizontal = TRUE, margin = list(l = 10, r = 10, b = 0, t = 0), col=(c("#E6E6E6", "#727272")))
+legend(26, 3, c("Internals", "Externals"), col=c("#727272", "#E6E6E6"), ncol=2, cex=1.7, bty = "n", fill = c("#727272", "#E6E6E6"))
+
+pulls_summary <- read.csv("Dataset/hubot/merged_pull_requests_summary.csv", colClasses=c(NA, NA, NA, NA, NA, NA, "Date", NA, NA, NA, NA, NA))
+internals <- subset(pulls_summary, user_type == "Internals")
+externals <- subset(pulls_summary, user_type == "Externals")
+internals_sum <- count(internals, "user_login")
+externals_sum <- count(externals, "user_login")
+mar.default <- c(4,1,4,1) + 0.1
+par(mar = mar.default + c(0, 5, 0, 0)) 
+boxplot(externals_sum$freq, internals_sum$freq, ylim=c(0, 103), xlab="# Occurrences", las = 1, outline = FALSE, cex.lab=2, cex.axis = 1.5, horizontal = TRUE, margin = list(l = 10, r = 10, b = 0, t = 0), col=(c("#E6E6E6", "#727272")))
+legend(26, 3, c("Internals", "Externals"), col=c("#727272", "#E6E6E6"), ncol=2, cex=1.7, bty = "n", fill = c("#727272", "#E6E6E6"))
+
+pulls_summary <- read.csv("Dataset/linguist/merged_pull_requests_summary.csv", colClasses=c(NA, NA, NA, NA, NA, NA, "Date", NA, NA, NA, NA, NA))
+internals <- subset(pulls_summary, user_type == "Internals")
+externals <- subset(pulls_summary, user_type == "Externals")
+internals_sum <- count(internals, "user_login")
+externals_sum <- count(externals, "user_login")
+mar.default <- c(4,1,4,1) + 0.1
+par(mar = mar.default + c(0, 5, 0, 0)) 
+boxplot(externals_sum$freq, internals_sum$freq, ylim=c(0, 103), xlab="# Occurrences", las = 1, outline = FALSE, cex.lab=2, cex.axis = 1.5, horizontal = TRUE, margin = list(l = 10, r = 10, b = 0, t = 0), col=(c("#E6E6E6", "#727272")))
+legend(26, 3, c("Internals", "Externals"), col=c("#727272", "#E6E6E6"), ncol=2, cex=1.7, bty = "n", fill = c("#727272", "#E6E6E6"))
+
+par(op) # Leave the last plot
+op <- par(usr=c(0,1,0,1), # Reset the coordinates
+          xpd=NA)         # Allow plotting outside the plot region
+# 1000, 750
+dev.copy2eps(file="Images/days_process_pull_r.eps", width = 10, height = 7.5)
+dev.off()
+
